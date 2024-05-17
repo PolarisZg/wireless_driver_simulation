27b97c65875c1ad4e74a17002600be70c8fce2e3
zgBCQ <hbczqbc@163.com>
2024-05-17 17:27:44 +0800

    add mirror website download openwrt image and fs

--- start_qemu_openwrt.sh
+++ start_qemu_openwrt.sh
@@ -1,5 +1,11 @@
 set -x
 
+# wget https://mirrors.aliyun.com/openwrt/releases/21.02.0/targets/armvirt/64/openwrt-21.02.0-armvirt-64-Image-initramfs
+
+# wget https://mirrors.aliyun.com/openwrt/releases/21.02.0/targets/armvirt/64/openwrt-21.02.0-armvirt-64-rootfs-ext4.img.gz
+
+# gzip -d openwrt-21.02.0-armvirt-64-rootfs-ext4.img.gz 
+
 QEMUPATH=$1
 
 $QEMUPATH -m 1024 -smp 2 -cpu cortex-a57 -M virt -nographic -kernel openwrt-21.02.0-armvirt-64-Image-initramfs -drive if=none,file=openwrt-21.02.0-armvirt-64-rootfs-ext4.img,id=hd0 -device virtio-blk-device,drive=hd0 
