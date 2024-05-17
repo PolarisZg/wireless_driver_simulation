d3ed2c4faffe5f5560dbc55aa17235d5874a7313
zgBCQ <hbczqbc@163.com>
2024-05-17 16:58:35 +0800

    feat : add scrript to prepare dev tools

---
+++ preparelibs.sh
@@ -0,0 +1,12 @@
+set -x
+sudo apt install libfdt-dev 
+sudo apt install libpixman-1-dev
+sudo apt install libglib2.0-dev
+sudo apt install pkg-config 
+sudo apt install zlib1g-dev 
+sudo apt install build-essential 
+sudo apt install ninja-build 
+sudo apt install python3
+sudo apt install python-is-python3
+sudo apt install libusb-dev 
+sudo apt-get install python3-venv
