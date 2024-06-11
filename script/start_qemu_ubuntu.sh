1dd839589039bf88462ebec06c7141fdd0f1153d
zgBCQ <hbczqbc@163.com>
2024-06-11 16:36:56 +0800

    add : start qemu ubuntu

---
+++ script/start_qemu_ubuntu.sh
@@ -0,0 +1,18 @@
+
+# sudo apt install bridge-utils iptables dnsmasq
+# cp qemu-ifup ../qemu_simudevice/build/qemu-bundle/usr/local/etc/
+# sudo chmod 755 ../qemu_simudevice/build/qemu-bundle/usr/local/etc/qemu-ifup
+
+../qemu_simudevice/build/qemu-system-x86_64 \
+-m 2G \
+-drive format=qcow2,file=ubuntu.qcow2 \
+-enable-kvm \
+-net tap -net nic \
+-vnc 10.129.19.237:0 \
+-device hellodev-pci \
+-nographic
+# -cdrom ./ubuntu-22.04.4-desktop-amd64.iso
+
+# 下方的暂时无用
+# -device virtio-net-pci,netdev=lan \
+# -netdev tap,id=lan,ifname=ubuntu_tap0,script=no,downscript=no \
