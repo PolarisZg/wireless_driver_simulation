a513fe0b414b2ccb917f30b07a6fd9facdcec8fe
zgBCQ <hbczqbc@163.com>
2024-06-11 16:33:18 +0800

    format code

--- script/start_qemu_openwrt.sh
+++ script/start_qemu_openwrt.sh
@@ -8,4 +8,14 @@ set -x
 
 QEMUPATH=$1
 
-$QEMUPATH -m 1024 -smp 2 -cpu cortex-a57 -M virt -nographic -kernel openwrt-21.02.0-armvirt-64-Image-initramfs -drive if=none,file=openwrt-21.02.0-armvirt-64-rootfs-ext4.img,id=hd0 -device virtio-blk-device,drive=hd0 
+sudo \
+$QEMUPATH -M virt -nographic \
+-m 1024 \
+-cpu cortex-a57 -smp 2 \
+-kernel openwrt-21.02.0-armvirt-64-Image-initramfs \
+-blockdev driver=raw,node-name=hd0,cache.direct=on,file.driver=file,file.filename=openwrt-21.02.0-armvirt-64-rootfs-ext4.img \
+-device virtio-blk-pci,drive=hd0 \
+-device virtio-net-pci,netdev=lan \
+-netdev tap,id=lan,ifname=ubuntu_tap0,script=no,downscript=no \
+# -netdev type=tap,id=nic1,ifname=ubuntu_tap0,script=no,downscript=no \
+# -device virtio-net-pci,disable-legacy=on,disable-modern=off,netdev=nic1,mac=ba:ad:1d:ea:01:02 
