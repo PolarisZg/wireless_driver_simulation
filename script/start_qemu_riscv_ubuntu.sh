075b332aaddc3e8ecf24a281eb28432d3fdebee5
zgBCQ <hbczqbc@163.com>
2024-06-13 16:12:37 +0800

    add : start riscv ubuntu img

---
+++ script/start_qemu_riscv_ubuntu.sh
@@ -0,0 +1,10 @@
+# uboot.elf download from sudo apt install uboot
+# cp /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf ./
+
+../qemu_simudevice/build/qemu-system-riscv64 \
+-machine virt -nographic -m 2048 -smp 4 \
+-drive file=ubuntu-24.04-preinstalled-server-riscv64.img,format=raw,if=virtio \
+-kernel ./uboot.elf \
+-device virtio-net-device,netdev=eth0 -netdev tap,id=eth0 \
+-device virtio-rng-pci \
+-device hellodev-pci
