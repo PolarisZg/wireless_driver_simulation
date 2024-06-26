3c9c77c2d1ff0fe1042138993a380939475451f1
zgBCQ <hbczqbc@163.com>
2024-06-26 14:57:29 +0800

    change : add sudo in start ubuntu on qemu

--- script/start_qemu_riscv_ubuntu.sh
+++ script/start_qemu_riscv_ubuntu.sh
@@ -1,7 +1,7 @@
 # uboot.elf download from sudo apt install uboot
 # cp /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf ./
 
-../qemu_simudevice/build/qemu-system-riscv64 \
+sudo ../qemu_simudevice/build/qemu-system-riscv64 \
 -machine virt -nographic -m 2048 -smp 4 \
 -drive file=ubuntu-24.04-preinstalled-server-riscv64.img,format=raw,if=virtio \
 -kernel ./uboot.elf \
