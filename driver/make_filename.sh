56a89fea73c08586b7af0a0f42d34e104769e6b0
zgBCQ <hbczqbc@163.com>
2024-07-01 11:01:59 +0800

    add .sh script to get linux kernel version

--- driver/make_filename.sh
+++ driver/make_filename.sh
@@ -1,6 +1,8 @@
 CURRENT_DIR=$(pwd)
 
-KERNEL_DIR=/usr/src/linux-headers-6.5.0-35-generic
+KERNEL_VERSION=$(uname -r)
+KERNEL_DIR=/usr/src/linux-headers-$KERNEL_VERSION
+
 set -x
 filename_temp=$(basename -- "$1")
 FILE_NAME=${filename_temp%.c}
