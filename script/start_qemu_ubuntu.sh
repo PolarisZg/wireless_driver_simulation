65abf9d1410945cc27741e1f5cf5312d1a3be370
zgBCQ <hbczqbc@163.com>
2024-07-08 18:11:45 +0800

    add : add sudo in x86 qemu ubuntu

--- script/start_qemu_ubuntu.sh
+++ script/start_qemu_ubuntu.sh
@@ -3,13 +3,14 @@
 # cp qemu-ifup ../qemu_simudevice/build/qemu-bundle/usr/local/etc/
 # sudo chmod 755 ../qemu_simudevice/build/qemu-bundle/usr/local/etc/qemu-ifup
 
-../qemu_simudevice/build/qemu-system-x86_64 \
+sudo ../qemu_simudevice/build/qemu-system-x86_64 \
 -m 2G \
 -drive format=qcow2,file=ubuntu.qcow2 \
 -enable-kvm \
 -net tap -net nic \
 -vnc 10.129.19.237:0 \
 -device hellodev-pci \
+-device edu \
 -nographic
 # -cdrom ./ubuntu-22.04.4-desktop-amd64.iso
 
