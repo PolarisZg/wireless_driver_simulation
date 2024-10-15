aef724986ac7a19b1a7aef0353e43621241eda04
zgBCQ <hbczqbc@163.com>
2024-10-15 17:04:10 +0800

    update : add pr_info in write32

--- driver/driver_wireless/wireless_hif.c
+++ driver/driver_wireless/wireless_hif.c
@@ -2,5 +2,6 @@
 
 void wireless_hif_write32(struct wireless_simu *priv, u32 address, u32 data)
 {
+    pr_info("%s : hif write32 %p mmio %08x addr %08x data \n", WIRELESS_SIMU_DEVICE_NAME, priv->mmio_addr, address, data);
     iowrite32(data, priv->mmio_addr + address);
 }
