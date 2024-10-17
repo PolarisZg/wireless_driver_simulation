d0c6a6637824dfc2253c19bb2621aec736c6f755
zgBCQ <hbczqbc@163.com>
2024-10-17 09:42:11 +0800

    add : define no irq

--- driver/driver_wireless/wireless_ce.h
+++ driver/driver_wireless/wireless_ce.h
@@ -3,6 +3,9 @@
 
 #include "wireless.h"
 
+/* no interrupt on copy completion */
+#define CE_ATTR_DIS_INTR		8
+
 int wireless_simu_ce_send(struct wireless_simu *priv, struct sk_buff *skb);
 
 #endif
