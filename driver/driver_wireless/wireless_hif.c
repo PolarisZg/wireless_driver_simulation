4b9443a189fc22145dc3f73b2f8e19e17fbabb67
Baichuan Qi <zghbqbc@gmail.com>
2024-11-27 14:50:53 +0800

    driver : hif : Add hif read function
    在驱动中添加对硬件寄存器的 32bit读方法，使用该读方法时，需要注意
    以下几点：
    
    1. 寄存器地址不要使用直接的数字，一般可以使用宏定义，最好使用枚举体加
    宏定义处理的方式来给出寄存器地址，便于扩展
    
    Signed-off-by: Baichuan Qi <zghbqbc@gmail.com>

--- driver/driver_wireless/wireless_hif.c
+++ driver/driver_wireless/wireless_hif.c
@@ -5,3 +5,12 @@ void wireless_hif_write32(struct wireless_simu *priv, u32 address, u32 data)
     pr_info("%s : hif write32 %p mmio %08x addr %08x data \n", WIRELESS_SIMU_DEVICE_NAME, priv->mmio_addr, address, data);
     iowrite32(data, priv->mmio_addr + address);
 }
+
+u32 wireless_hif_read32(struct wireless_simu *priv, u32 address)
+{
+    u32 val = 0;
+
+    val = ioread32(priv->mmio_addr + address);
+
+    return val;
+}
--- driver/driver_wireless/wireless_hif.h
+++ driver/driver_wireless/wireless_hif.h
@@ -5,4 +5,6 @@
 
 void wireless_hif_write32(struct wireless_simu *priv, u32 address, u32 data);
 
+u32 wireless_hif_read32(struct wireless_simu *priv, u32 address);
+
 #endif
