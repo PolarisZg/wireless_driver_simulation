c081e10828464f4dd7743532d43929d90db38f8a
Baichuan Qi <zghbqbc@gmail.com>
2024-11-27 15:31:56 +0800

    driver : dprx : add forward deciaration for wireless_simu
    为最大的那个结构体添加前置声明；实际上该声明添加在哪个头文件中
    都无所谓的
    
    Signed-off-by: Baichuan Qi <zghbqbc@gmail.com>

--- driver/driver_wireless/wireless_dprx.h
+++ driver/driver_wireless/wireless_dprx.h
@@ -2,5 +2,7 @@
 #define WIRELESS_SIMU_DPRX
 #include "wireless.h"
 
+struct wireless_simu; // 总之需要在一个.h文件中前向声明一次这个结构体，才能被其他的各个模块使用
+
 int wireless_simu_dp_rx_crypto_mic_len(struct wireless_simu *priv, enum hal_encrypt_type enctype);
 #endif
