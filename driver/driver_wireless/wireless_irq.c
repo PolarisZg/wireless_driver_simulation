430ff738fe8f9127ca856b98171f1fed1ae316b7
Baichuan Qi <zghbqbc@gmail.com>
2024-11-27 14:42:22 +0800

    driver : irq : Add Interrupt module in driver
    添加在驱动中添加对中断的支持，关于中断功能的写法，有如下描述：
    
    1. 所有的中断状态写到枚举 `enum WIRELESS_SIMU_ENUM_IRQ_STATUS` 中，
    以 WIRELESS_SIMU_IRQ_STATU_xxxx 格式书写
    
    2. 中断相关的寄存器位置写到 wireless.h 文件的 `enum HAL_ENUM_REG_BASIC`
    中，所有的基础寄存器以 WIRELESS_REG_BASIC_xxx 格式书写，以
    HAL_BASIC_REG(WIRELESS_REG_BASIC_xxx) 的格式使用，如 write / read
    
    Signed-off-by: Baichuan Qi <zghbqbc@gmail.com>

---
+++ driver/driver_wireless/wireless_irq.c
@@ -0,0 +1,24 @@
+#include "wireless.h"
+
+irqreturn_t wireless_simu_irq_handler(int irq, void *dev)
+{
+    struct pci_dev *pdev = dev;
+    struct wireless_simu *priv = pci_get_drvdata(pdev);
+
+    u32 irq_status = wireless_hif_read32(priv, HAL_BASIC_REG(WIRELESS_REG_BASIC_IRQ_STATUS));
+    pr_info("%s : interrupt start, irq_status %08x \n", WIRELESS_SIMU_DEVICE_NAME, irq_status);
+
+    switch (irq_status)
+    {
+    case WIRELESS_SIMU_IRQ_STATU_SRNG_DST_DMA_TEST_RING_0:
+        tasklet_schedule(&priv->st_dst.pipes[0].intr_tq);
+        break;
+    default:
+        break;
+    }
+
+    /* 清中断 向状态寄存器写0 */
+    wireless_hif_write32(priv, HAL_BASIC_REG(WIRELESS_REG_BASIC_IRQ_STATUS), 0);
+
+    return IRQ_HANDLED;
+}
