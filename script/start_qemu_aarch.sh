0acb0a0e77a6ee96f44ed3303b97ba13e38f55c3
zgBCQ <hbczqbc@163.com>
2025-03-13 17:07:01 +0800

    add : script : aarch qemu start with compile driver ko files
    修改 aarch qemu 的启动脚本, 在启动之前编译驱动及内核

--- script/start_qemu_aarch.sh
+++ script/start_qemu_aarch.sh
@@ -1,18 +1,31 @@
 set -x
 
 ORIGINAL_DIR=$(pwd)
+MODULES_DIR=$ORIGINAL_DIR/modules
+TMPROOTFS_DIR=$ORIGINAL_DIR/tmprootfs
 
-cd ../linux-stable
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all -j$(nproc)
-cd $ORIGINAL_DIR
+mkdir -p $MODULES_DIR
+mkdir -p $TMPROOTFS_DIR
 
-cd ../driver/driver_wireless/cp_to_kernel.sh
+cd ../driver/driver_wireless
 ./cp_to_kernel.sh
 cd $ORIGINAL_DIR
 
-cd ../qemu_simudevice//build
+cd ../linux-stable
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all -j$(nproc)
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules_install INSTALL_MOD_PATH=$MODULES_DIR > /dev/null
+cd $ORIGINAL_DIR
+
+
+cd ../qemu_simudevice/build
 ninja
 cd $ORIGINAL_DIR
+
+sudo mount -t ext3 aarchrootfs.etx3 $TMPROOTFS_DIR
+sudo cp -r $MODULES_DIR/lib/modules/* $TMPROOTFS_DIR/lib/modules/
+sudo umount $TMPROOTFS_DIR
+rm -rf $TMPROOTFS_DIR
+rm -rf $MODULES_DIR
 
 ../qemu_simudevice/build/qemu-system-aarch64 -M virt -cpu cortex-a53 -m 16G \
 -nographic \
