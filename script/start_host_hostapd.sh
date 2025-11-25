8dcbd624e7e1bf9038905309fc00a194e843279d
zgBCQ <hbczqbc@163.com>
2025-11-25 11:36:39 +0800

    update : script 修改脚本：代码提示、ap创建、qemu运行日志

--- script/start_host_hostapd.sh
+++ script/start_host_hostapd.sh
@@ -3,6 +3,15 @@ set -x
 
 rm -r -f ./hostapd.conf
 
+sudo systemctl mask wpa_supplicant
+sudo systemctl stop wpa_supplicant
+
+sudo ip addr flush dev wlan0
+sleep 0.5
+sudo ifconfig wlan0 down
+sleep 1
+sudo ifconfig wlan0 up
+
 WIFI_CARD_NAME=wlx08beac26814c
 
 sudo killall hostapd
@@ -32,3 +41,5 @@ sudo ifconfig apmon up
 sudo hostapd ./hostapd.conf
 
 rm -r -f ./hostapd.conf
+
+# sudo ifconfig wlan0 10.5.5.1 netmask 255.255.255.0
